3f8c36a5bd23836759dc45823891e81ca1ff75bc
isXander <xandersmith2008@gmail.com>
2021-12-12 23:02:59 +0000

    bump nightconfig in /processor

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -4,6 +4,6 @@ dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.6.0-1.0.1")
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":annotations"))
-    implementation("com.electronwill.night-config:core:3.6.4")
-    implementation("com.electronwill.night-config:json:3.6.4")
+    implementation("com.electronwill.night-config:core:3.6.5")
+    implementation("com.electronwill.night-config:json:3.6.5")
 }
