5fe808df9e14ad2df9c1069024f9084a7fb4147f
isXander <xandersmith2008@gmail.com>
2021-12-13 22:18:05 +0000

    update copyright

--- processor/src/main/kotlin/dev/isxander/evergreenhud/processor/ElementProcessor.kt
+++ processor/src/main/kotlin/dev/isxander/evergreenhud/processor/ElementProcessor.kt
@@ -1,18 +1,9 @@
 /*
- * EvergreenHUD - A mod to improve on your heads-up-display.
- * Copyright (C) isXander [2019 - 2021]
+ * EvergreenHUD - A mod to improve your heads-up-display.
+ * Copyright (c) isXander [2019 - 2021].
  *
- * This program comes with ABSOLUTELY NO WARRANTY
- * This is free software, and you are welcome to redistribute it
- * under the certain conditions that can be found here
- * https://www.gnu.org/licenses/lgpl-2.1.en.html
- *
- * If you have any questions or concerns, please create
- * an issue on the github page that can be found here
- * https://github.com/isXander/EvergreenHUD
- *
- * If you have a private concern, please contact
- * isXander @ business.isxander@gmail.com
+ * This work is licensed under the CC BY-NC-SA 4.0 License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0
  */
 
 package dev.isxander.evergreenhud.processor
