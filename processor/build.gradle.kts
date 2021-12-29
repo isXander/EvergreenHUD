0656064ddac109c8b735232b430ac873b472f2d5
isXander <xandersmith2008@gmail.com>
2021-12-29 13:20:51 +0000

    update ksp

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -12,7 +12,7 @@ apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
 val implementation by configurations
 
 dependencies {
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.0-1.0.2")
+    implementation("com.google.devtools.ksp:symbol-processing-api:$kotlinVersion-1.0.+")
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":utils"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
